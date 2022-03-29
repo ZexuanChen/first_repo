48f9304cc22847a6d5eeb79d1e4cf182a08cc70c
twilight <102341593+ZexuanChen@users.noreply.github.com>
2022-03-29 19:54:06 +0800

    Add files via upload

---
+++ 拓扑排序.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5+5; 
+int n, m, cnt, in[maxn];
+
+struct Edge{
+    int to, w, next;
+} edge[maxn];
+int head[maxn];
+
+void init() 
+{
+    for (int i = 1; i <= n; i++) head[i] = -1;
+}
+
+void add_edge(int u, int v)
+{
+    edge[cnt].to = v;
+    edge[cnt].next = head[u];
+    head[u] = cnt++;
+}
+
+void topSort()
+{
+	priority_queue<int, vector<int>, greater<int> > q;
+    for (int i=1; i<=n; i++) if (in[i]==0) q.push(i);
+    
+    vector<int> ans; //ans为拓扑序列 
+    while(!q.empty()) {
+        int p = q.top(); q.pop(); //选一个入度为0的点，出队列 
+        ans.push_back(p);
+        for(int i=head[p]; i != -1; i=edge[i].next) {
+            int j=edge[i].to;
+            in[j]--;
+            if(in[j]==0) q.push(j);  
+        }
+    }
+    
+    if (ans.size() == n) {
+        for(int i=0; i<ans.size(); i++) printf("%d ", ans[i]);
+        printf("\n");
+    }
+    else printf("No Answer!\n");   //  ans 中的长度与n不相等，就说明无拓扑序列
+}
+
+int main()
+{
+	//建树并统计入度
+	init();
+	topSort();
+}
